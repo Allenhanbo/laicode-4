be67c9fa36bd29763b9190669e5cd9f394cfaa2a
Jiapei Liang <liang@jiapei.io>
2017-09-29 09:34:54 -0400

    Added LongestSubstringWithoutRepeatingCharacters.java to Class08

--- Class08/LongestSubstringWithoutRepeatingCharacters.java
+++ Class08/LongestSubstringWithoutRepeatingCharacters.java
@@ -1,8 +1,11 @@
 /**
  *
+ * Given a string, find the longest substring without any repeating characters
+ * and return the length of it. The input string is guaranteed to be not null.
  *
- *
- *
+ * Example
+ *    the longest substring without repeating letters for "bcdfbd" is "bcdf",
+ *    we should return 4 in this case.
  *
  **/
 
